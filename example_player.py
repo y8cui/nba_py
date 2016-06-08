7ecc1193167e282a2805a9109341f11d4eb00d53
y8cui <cuiyuan51@gmail.com>
2016-06-08 00:21:25 -0700

    test
    test

--- example_player.py
+++ example_player.py
@@ -1,4 +1,4 @@
-from __future__ import print_function
+
 from nba_py import player
 
 # endpoint currently disabled on stats.nba.com
@@ -8,8 +8,3 @@ from nba_py import player
 ap = player.PlayerList()
 print(ap.info())
 
-pc = player.PlayerSummary('203507')
-print(pc.headline_stats())
-
-p_cstats = player.PlayerCareer('201939')
-print(p_cstats.regular_season_career_totals())
